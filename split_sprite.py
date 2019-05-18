0a259a5e05f9c33dea7d48a1db498fd0a58de42e
Rong Ying <kohrongying@gmail.com>
2019-05-18 23:08:05 +0800

    Add python scripts to split sprites.

--- split_sprite.py
+++ split_sprite.py
@@ -1,21 +1,54 @@
 from PIL import Image
+import os
 
-image = Image.open('sample.png')
-# print(image.size) # (64, 64)
-w, h = image.size
-size_x = 32
-size_y = 16
+## Save in '001/v1/001_01.png' format
+def filepath_to_save(original_path, part):
+  path_array = original_path.split("/")
+  serial_no = path_array[-1].split(".")[0]
+  final_path = '/'.join(path_array[:-1])
+  final_path += f'/{serial_no}_{str(part).zfill(2)}.png'
+  return final_path
 
-cols = int(w / size_x)
-rows = int(h / size_y)
+import subprocess
 
-count = 1
-for i in range(rows):
-  for j in range(cols):
-    xcoor_start = j * size_x
-    ycoor_start = i * size_y
-    xcoor_end = (j+1) * size_x
-    ycoor_end = (i+1) * size_y
-    image.crop((xcoor_start, ycoor_start, xcoor_end, ycoor_end)).save('{}.png'.format(count))
-    # print(f"Created num {count}")
-    count += 1
+## Convert image to one with fewer colors
+def convert_image_colors(filename):
+  command = f'cp {filename} {filename[:-4]}_original.png'
+  subprocess.run(command.split(" "))
+  command = f'convert {filename[:-4]}_original.png +dither -colors 5 {filename}'
+  subprocess.run(command.split(" "))
+
+## Crop image into 8x16 chunks and save them
+def main(filename):
+  image = Image.open(filename)
+  w, h = image.size #(64, 64)
+  size_x = 8
+  size_y = 16
+
+  cols = int(w / size_x)
+  rows = int(h / size_y)
+
+  count = 1
+
+  for i in range(rows):
+    for j in range(cols):
+      xcoor_start = j * size_x
+      ycoor_start = i * size_y
+      xcoor_end = (j+1) * size_x
+      ycoor_end = (i+1) * size_y
+
+      cropped_image = image.crop((xcoor_start, ycoor_start, xcoor_end, ycoor_end))
+      cropped_image.save(filepath_to_save(filename, count))
+      count += 1
+
+if __name__ == "__main__":
+  sprite_dir = os.path.join(os.getcwd(), "sprites/kanto")
+
+  for serial_no in os.listdir(sprite_dir):
+    serial_no_dir = os.path.join(sprite_dir, serial_no)
+
+    for i in range(1, 3):
+      indiv_image_dir = os.path.join(serial_no_dir, f'v{i}')
+      image_path = os.path.join(indiv_image_dir, os.listdir(indiv_image_dir)[0])
+      convert_image_colors(image_path)
+      main(image_path)
--- split_spritesheet.py
+++ split_spritesheet.py
@@ -1,21 +1,59 @@
 from PIL import Image
+import argparse
+import os 
+import math
 
-image = Image.open('sprite.png')
-# print(image.size) # (695, 2144)
-w, h = image.size
-border = 5
-size = 64
-count = 1
+def create_output_dest_if_not_found(out):
+  path = os.path.join(os.getcwd(), out)
+  if not os.path.exists(path) and not os.path.isdir(path):
+    os.makedirs(path)
 
-cols = int(w / (border+size))
-rows = int(h / (border+size))
+def main(params):
+  filepath = params['filepath']
+  region = params['region']
 
-for i in range(rows):
-  for j in range(cols):
-    xcoor_start = j * (size+border) + border
-    ycoor_start = i * (size+border) + border
-    xcoor_end = (j+1) * (size+border)
-    ycoor_end = (i+1) * (border+size)
-    image.crop((xcoor_start, ycoor_start, xcoor_end, ycoor_end)).save('img/{}.png'.format(count))
-    # print(f"Created num {count}")
-    count += 1
+  image = Image.open(filepath)
+  # print(image.size) # (695, 2144)
+
+  w, h = image.size
+  border = 5
+  size = 64
+  count = 1
+
+  cols = int(w / (border+size))
+  rows = int(h / (border+size))
+
+  for i in range(rows):
+    for j in range(cols):
+      xcoor_start = j * (size+border) + border
+      ycoor_start = i * (size+border) + border
+      xcoor_end = (j+1) * (size+border)
+      ycoor_end = (i+1) * (border+size)
+      cropped_image = image.crop((xcoor_start, ycoor_start, xcoor_end, ycoor_end))
+
+      serial_no = math.ceil(count/2)
+      if count % 2 == 0: # if even
+        version = 'v2'
+      else:
+        version = 'v1'
+
+      serial_no = str(serial_no)
+      filepath_to_save = f'sprites/{region}/{serial_no.zfill(3)}/{version}'
+      create_output_dest_if_not_found(filepath_to_save)
+
+      cropped_image.save(f'{filepath_to_save}/{serial_no.zfill(3)}.png')
+      
+      count += 1
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  
+  parser.add_argument('-f','--file', dest='filepath', type=str, help='path of the sprite file to split')
+  
+  parser.add_argument('-r','--region', dest='region', type=str, help='pokemon region')
+
+  args = parser.parse_args()
+  params = vars(args)
+
+  main(params)
